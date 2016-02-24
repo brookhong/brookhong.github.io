5d9f05acb7f7ee027fe3f944b2409469ad5ba5b3
brook hong <hzgmaxwell@hotmail.com>
2016-02-25 00:54:29 +0800

    test ga event

--- assets/js/default.js
+++ assets/js/default.js
@@ -1,3 +1,16 @@
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+function brook_ga() {
+    if (window.location.origin === "https://brookhong.github.io") {
+        ga.apply(ga, arguments);
+    }
+}
+brook_ga('create', 'UA-64048030-1', 'auto');
+brook_ga('send', 'pageview', { 'page': window.location.href, 'title': document.title });
+
 function isEnglish() {
     return ($('#menu a[section=profile]').html() == "About");
 }
@@ -38,6 +51,7 @@ if(isHomePage()) {
         var h2 = this;
         if ($(h2).is(':visible')) {
             $("<li><a href='javascript:void(0);'>" + $(this).text() + "</a></li>").appendTo('#toc ul').click(function() {
+                brook_ga('send', 'event', 'toc_click', window.location.href, $(this).text());
                 $('html, body').animate({
                     scrollTop: $(h2).offset().top
                 }, 500);
